d29fc051d9af66ed99ec3da3835ee304d9d6ba60
Rildo Franco <rildowilliam2017@gmail.com>
2021-04-30 17:21:45 +0000

    init

--- app/src/main/java/com/example/pedrapapeltesoura/TelaOpcoes.java
+++ app/src/main/java/com/example/pedrapapeltesoura/TelaOpcoes.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class TelaOpcoes extends AppCompatActivity {
     ImageView imgPedra;
     ImageView imgPapel;
     ImageView imgTesoura;
     ImageView imgComputador;
+    TextView resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +24,48 @@ public class TelaOpcoes extends AppCompatActivity {
         imgPapel = (ImageView) findViewById(R.id.imgPapel);
         imgTesoura = (ImageView) findViewById(R.id.imgTesoura);
         imgComputador = (ImageView) findViewById(R.id.imageView);
+        resultado = (TextView) findViewById(R.id.textView2);
+
+        int [] minhasImagens = new int[]{R.drawable.papel, R.drawable.pedra, R.drawable.tesoura};
+        Random r = new Random();
 
         imgPedra.setOnClickListener(v->{
+            int valor = r.nextInt(minhasImagens.length);
+            imgComputador.setImageResource(minhasImagens[valor]);
 
+            if (minhasImagens[valor] == R.drawable.papel){
+                resultado.setText("Você perdeu!");
+            } else if (minhasImagens[valor] == R.drawable.pedra){
+                resultado.setText("Empate!");
+            } else if (minhasImagens[valor] == R.drawable.tesoura){
+                resultado.setText("Você venceu!");
+            }
+        });
+
+        imgPapel.setOnClickListener(v->{
+            int valor = r.nextInt(minhasImagens.length);
+            imgComputador.setImageResource(minhasImagens[valor]);
+
+            if (minhasImagens[valor] == R.drawable.papel){
+                resultado.setText("Empate!");
+            } else if (minhasImagens[valor] == R.drawable.pedra){
+                resultado.setText("Você venceu!");
+            } else if (minhasImagens[valor] == R.drawable.tesoura){
+                resultado.setText("Você perdeu!");
+            }
+        });
+
+        imgTesoura.setOnClickListener(v->{
+            int valor = r.nextInt(minhasImagens.length);
+            imgComputador.setImageResource(minhasImagens[valor]);
+
+            if (minhasImagens[valor] == R.drawable.papel){
+                resultado.setText("Você venceu!");
+            } else if (minhasImagens[valor] == R.drawable.pedra){
+                resultado.setText("Você perdeu!");
+            } else if (minhasImagens[valor] == R.drawable.tesoura){
+                resultado.setText("Empate!");
+            }
         });
     }
 }
